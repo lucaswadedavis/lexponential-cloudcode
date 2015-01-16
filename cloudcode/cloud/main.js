0e06e934ed12d3dacfbc065965da9feecb24d4c1
Hack Reactor Students <accounts@hackreactor.com>
2015-01-15 20:36:19 -0800

    moar codez

--- cloudcode/cloud/main.js
+++ cloudcode/cloud/main.js
@@ -5,6 +5,23 @@ Parse.Cloud.define("chop", function(request, response) {
   var words=JSON.parse(request.body);
   words=words.words;
   words=words.split(" ");
-  response.success(words.length);
-  
+
+  //make the Lexicon constructor function (class)
+  var Lexicon=Parse.Object.extend("Lexicon");
+  var lexicon=new Lexicon();
+
+  for (var i=0;i<words.length;i++){
+    lexicon.set(words[i],Math.floor(1000*Math.random() ));
+  }
+
+  lexicon.save(null,{
+    success:function(lexicon){
+      response.success(lexicon);
+    },
+    error:function(lexicon,error){
+      response.success(error);
+    }
+  });
+
+
 });
