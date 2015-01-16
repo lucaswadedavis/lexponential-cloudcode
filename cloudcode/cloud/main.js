158a096a2deb9f57ce99541fe2f3b7ebe43eeb27
Luke Davis <lukefable@gmail.com>
2015-01-16 06:36:46 +0000

    don't know what the hell is wrong with the requests starting on line 5 and continuing for... ever...

--- cloudcode/cloud/main.js
+++ cloudcode/cloud/main.js
@@ -1,6 +1,35 @@
 
 // Use Parse.Cloud.define to define as many cloud functions as you want.
 // For example:
+
+Parse.Cloud.afterSave("Todo", function(request, response) {
+  var words=JSON.parse(request);
+  console.log(request.content);
+  console.log("Luke!");
+  words=words.content;
+  words=words.split(" ");
+
+  //make the Lexicon constructor function (class)
+  var Word=Parse.Object.extend("Word");
+
+  for (var i=0;i<words.length;i++){
+    var word=new Word();
+    word.set("count",Math.floor(1000*Math.random() ));
+    word.set("english",words[i]);
+    word.save(null,{
+      success:function(res){
+        //response.success(res);
+      },
+      error:function(res,error){
+        response.success(error);
+      }
+    });
+  }
+
+  response.success();
+
+});
+
 Parse.Cloud.define("chop", function(request, response) {
   var words=JSON.parse(request.body);
   words=words.words;
