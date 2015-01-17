efa03e4e6c76df94c3e5a53eabfdb1038fb21477
Luke Davis <lukefable@gmail.com>
2015-01-17 22:24:38 +0000

    started the work of incrementing counts.

--- cloudcode/cloud/main.js
+++ cloudcode/cloud/main.js
@@ -4,18 +4,34 @@
 
 Parse.Cloud.afterSave("Todo", function(request, response) {
   var words;
-  //words=JSON.parse(request);
-  //console.log(request.content);
-  console.log("Luke!");
   words=request.object.get("content");
   words=words.split(" ");
 
-  //make the Lexicon constructor function (class)
   var Word=Parse.Object.extend("Word");
-
+  
   for (var i=0;i<words.length;i++){
+    //query for pre-existing copy
+    //var found=false;
+    var query = new Parse.Query(Word);
+    query.equalTo("english", words[i]);
+    query.find({
+      success: function(results) {
+        //alert("Successfully retrieved " + results.length + " scores.");
+        // Do something with the returned Parse.Object values
+        for (var i = 0; i < results.length; i++) { 
+          var count = results[i].get('count');
+          results[i].set('count',count+1);
+          results[i].save();
+          //var object = results[i];
+          //alert(object.id + ' - ' + object.get('playerName'));
+        }
+      },
+      error: function(error) {
+        //alert("Error: " + error.code + " " + error.message);
+      }
+    });
     var word=new Word();
-    word.set("count",Math.floor(1000*Math.random() ));
+    word.set("count",1);
     word.set("english",words[i]);
     word.save(null,{
       success:function(res){
@@ -31,6 +47,8 @@ Parse.Cloud.afterSave("Todo", function(request, response) {
 
 });
 
+//example of a custom route 
+/*
 Parse.Cloud.define("chop", function(request, response) {
   var words=JSON.parse(request.body);
   words=words.words;
@@ -56,3 +74,4 @@ Parse.Cloud.define("chop", function(request, response) {
   response.success();
 
 });
+*/
