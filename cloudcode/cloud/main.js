0f691c23f324584877070ea4e203a5329ca62914
Luke Davis <lukefable@gmail.com>
2015-03-14 21:37:43 +0000

    continued refactor

--- cloudcode/cloud/main.js
+++ cloudcode/cloud/main.js
@@ -1,61 +1,123 @@
 var gtkey=require('cloud/creds.js');
 
-Parse.Cloud.afterSave("Lexiome", function(request, response) {
-  //console.log(gtkey() );
-  var lexemes;
-  var languages={
-    "afrikaans":"af",
-    "albanian":"sq",
-    "arabic":"ar",
-    "azerbaijani":"az",
-    "basque":"eu",
-    "bengali":"bn",
-    "belarusian":"be",
-    "bulgarian":"bg",
-    "catalan":"ca",
-    "croatian":"hr",
-    "czech":"cs",
-    "danish":"da",
-    "dutch":"nl",
-    "english":"en",
-    "esperanto":"eo",
-    "estonian":"et",
-    "finnish":"fi",
-    "french":"fr",
-    "galician":"gl",
-    "georgian":"ka",
-    "german":"de",
-    "greek":"el",
-    "haitian Creole":"ht",
-    "hebrew":"iw",
-    "hindi":"hi",
-    "hungarian":"hu",
-    "icelandic":"is",
-    "irish":"ga",
-    "italian":"it",
-    "latin":"la",
-    "latvian":"lv",
-    "lithuanian":"lt",
-    "macedonian":"mk",
-    "norwegian":"no",
-    "persian":"fa",
-    "polish":"pl",
-    "portuguese":"pt",
-    "romanian":"ro",
-    "russian":"ru",
-    "serbian":"sr",
-    "slovak":"sk",
-    "slovenian":"sl",
-    "spanish":"es",
-    "swahili":"sw",
-    "swedish":"sv",
-    "turkish":"tr",
-    "ukrainian":"uk",
-    "urdu":"ur",
-    "welsh":"cy",
-    "yiddish":"yi"
-  };
+var languages={
+  "afrikaans":"af",
+  "albanian":"sq",
+  "arabic":"ar",
+  "azerbaijani":"az",
+  "basque":"eu",
+  "bengali":"bn",
+  "belarusian":"be",
+  "bulgarian":"bg",
+  "catalan":"ca",
+  "croatian":"hr",
+  "czech":"cs",
+  "danish":"da",
+  "dutch":"nl",
+  "english":"en",
+  "esperanto":"eo",
+  "estonian":"et",
+  "finnish":"fi",
+  "french":"fr",
+  "galician":"gl",
+  "georgian":"ka",
+  "german":"de",
+  "greek":"el",
+  "haitian Creole":"ht",
+  "hebrew":"iw",
+  "hindi":"hi",
+  "hungarian":"hu",
+  "icelandic":"is",
+  "irish":"ga",
+  "italian":"it",
+  "latin":"la",
+  "latvian":"lv",
+  "lithuanian":"lt",
+  "macedonian":"mk",
+  "norwegian":"no",
+  "persian":"fa",
+  "polish":"pl",
+  "portuguese":"pt",
+  "romanian":"ro",
+  "russian":"ru",
+  "serbian":"sr",
+  "slovak":"sk",
+  "slovenian":"sl",
+  "spanish":"es",
+  "swahili":"sw",
+  "swedish":"sv",
+  "turkish":"tr",
+  "ukrainian":"uk",
+  "urdu":"ur",
+  "welsh":"cy",
+  "yiddish":"yi"
+};
+
+addLexeme = function(lexeme, count, opts){
+  //query for pre-existing copy
+      
+  //console.log('inside the iife');
+  //console.log(lexeme);
+  var query = new Parse.Query(Lexeme);
+  query.equalTo("lexeme", lexeme);
+  query.find({
+  success:function(res){
+    //console.log(res.length);
+    if (!res.length){
+      var newLexeme=new Lexeme();
+      //put translate command here
+      var url="https://www.googleapis.com/language/translate/v2";
+      url+="?key="+gtkey()+"&q="+lexeme+"&source="+opts.sourceLangAbbreviation+"&target="+opts.targetLangAbbreviation+"";
+      //console.log(url);
+      Parse.Cloud.httpRequest({
+        url: url,
+        success: function(httpResponse) {
+          console.log(httpResponse.text);
+          var res=JSON.parse(httpResponse.text);
+          var translation="no translation";
+          if (res.data.translations.length>0 && res.data.translations[0].translatedText){
+            translation=res.data.translations[0].translatedText;
+          }
+          //console.log('this next thing should be the translation:');
+          //console.log(english);
+          if (translation==="no translation"){
+            return;
+          }else {
+            newLexeme.set("sourceLang",opts.sourceLang);
+            newLexeme.set("targetLang",opts.targetLang)
+            newLexeme.set("lexeme",lexeme);
+            newLexeme.set("translation",opts.translation);
+            newLexeme.set('count',count);
+            newLexeme.set('owner',opts.user);
+            newLexeme.set('exposures',1)
+            newLexeme.save();
+          }
+
+          
+        },
+        error: function(httpResponse) {
+          console.error('Request failed with response code ' + httpResponse.status);
+        }
+      });
+      
+      
+    }
+    //what am I doing here?
+    for (var j=0;j<res.length;j++){
+      res[j].increment('count',count);
+      res[j].save(); 
+    }
+  }, error: function(res,err){
+    console.log(err);
+  }
+    
+  });
+
+};
+
   
+Parse.Cloud.afterSave("Lexiome", function(request, response) {
+  var lexemes;
   var sourceLang=request.object.get("sourceLang");
   var sourceLangAbbreviation=languages[sourceLang.toLowerCase()] ? languages[sourceLang.toLowerCase()] : "es" ;
   
@@ -72,74 +134,17 @@ Parse.Cloud.afterSave("Lexiome", function(request, response) {
 
   var Lexeme=Parse.Object.extend("Lexeme");
   
+  var opts = {};
+  opts.sourceLang = sourceLang;
+  opts.targetLang = targetLang;
+  opts.translation = translation;
+  opts.user = user;
+  opts.sourceLangAbbreviation = sourceLangAbbreviation;
+  opts.targetLangAbbreviation = targetLangAbbreviation;
+  
+  
   for (var key in uniques){
-    //query for pre-existing copy
-      (function(lexeme,count){
-        //console.log('inside the iife');
-        //console.log(lexeme);
-        var query = new Parse.Query(Lexeme);
-        query.equalTo("lexeme", lexeme);
-        query.find({
-        success:function(res){
-          //console.log(res.length);
-          if (!res.length){
-            var newLexeme=new Lexeme();
-            //put translate command here
-            var url="https://www.googleapis.com/language/translate/v2";
-            url+="?key="+gtkey()+"&q="+lexeme+"&source="+sourceLangAbbreviation+"&target="+targetLangAbbreviation+"";
-            //console.log(url);
-            Parse.Cloud.httpRequest({
-              url: url,
-              success: function(httpResponse) {
-                console.log(httpResponse.text);
-                var res=JSON.parse(httpResponse.text);
-                var translation="no translation";
-                if (res.data.translations.length>0 && res.data.translations[0].translatedText){
-                  translation=res.data.translations[0].translatedText;
-                }
-                //console.log('this next thing should be the translation:');
-                //console.log(english);
-                if (translation==="no translation"){
-                  return;
-                }else {
-                  newLexeme.set("sourceLang",sourceLang);
-                  newLexeme.set("targetLang",targetLang)
-                  newLexeme.set("lexeme",lexeme);
-                  newLexeme.set("translation",translation);
-                  newLexeme.set('count',count);
-                  newLexeme.set('owner',user);
-                  newLexeme.set('exposures',1)
-                  newLexeme.save();
-                }
-    
-                
-              },
-              error: function(httpResponse) {
-                console.error('Request failed with response code ' + httpResponse.status);
-              }
-            });
-            
-            
-          }
-          
-          for (var j=0;j<res.length;j++){
-            res[j].increment('count',count);
-            res[j].save(); 
-          }
-        }, error: function(res,err){
-          console.log(err);
-        }
-        
-      });
-    
-    }(key,uniques[key]));
-
-    /*
-    
-    
-    */
+    addLexeme(key, uniques[key], opts);
   }
-
-  //response.success();
 
 });
